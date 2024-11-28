aeea82821da5769d55591a37381af2e5c54d4a22
Igor <igopood33@gmail.com>
2024-11-28 22:11:29 +0100

    added index idx_subscriptions_active_created_at

---
+++ lab5/active-subscriptions-on-current-day-with-fk-id.sql
@@ -0,0 +1,15 @@
+SELECT 
+	CURRENT_DATE AS date,
+	ss.name AS subscription_state,
+	u.name || ' ' || u.surname AS customer
+FROM 
+	subscriptions s
+JOIN 
+	subscription_states ss ON s.fk_status_id = ss.id
+JOIN
+	customers c ON s.fk_customer_id = c.id
+JOIN
+	users u ON c.fk_user_id = u.id
+WHERE 
+	s.fk_status_id = 1
+	AND s.created_at <= NOW();
--- lab6/create_indexes.sql
+++ lab6/create_indexes.sql
@@ -7,4 +7,5 @@ CREATE INDEX idx_subscriptions_fk_status_id ON subscriptions (fk_status_id);
 CREATE INDEX idx_delivieries_fk_subscription_id ON deliveries (fk_subscription_id);
 CREATE INDEX idx_deliveries_subscription_date ON deliveries (fk_subscription_id, date DESC);
 CREATE INDEX idx_deliveries_fk_subscription_id ON deliveries (fk_subscription_id);
-CREATE INDEX idx_test_users_id_name_surname ON users (id, name, surname)
+CREATE INDEX idx_test_users_id_name_surname ON users (id, name, surname);
+CREATE INDEX idx_subscriptions_active_created_at ON subscriptions(created_at) WHERE fk_status_id = 1;
